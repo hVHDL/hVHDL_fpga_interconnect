a76aafcc20ae381c6b40028464b3c0c2f26b675f
jari <jaarhonka@yahoo.com>
2022-02-16 23:32:49 +0200

    implement a bus and test with fpga

--- fpga_interconnect_pkg.vhd
+++ fpga_interconnect_pkg.vhd
@@ -3,63 +3,195 @@ library ieee;
     use ieee.numeric_std.all;
 
 package fpga_interconnect_pkg is
-------------------------------------------------------------------------
+
     type fpga_interconnect_record is record
-        fpga_interconnect_is_done : boolean;
-        fpga_interconnect_is_requested : boolean;
+        data                           : std_logic_vector(15 downto 0);
+        address                        : std_logic_vector(15 downto 0);
+        data_write_is_requested_with_0 : std_logic;
+        data_read_is_requested_with_0  : std_logic;
     end record;
 
-    constant init_fpga_interconnect : fpga_interconnect_record := (false, false);
+    constant init_fpga_interconnect : fpga_interconnect_record := ((others => '1'), (others => '1'), '1', '1');
+
 ------------------------------------------------------------------------
-    procedure create_fpga_interconnect (
-        signal fpga_interconnect_object : inout fpga_interconnect_record);
+    function "and" ( left, right : fpga_interconnect_record)
+        return fpga_interconnect_record;
 ------------------------------------------------------------------------
-    procedure request_fpga_interconnect (
-        signal fpga_interconnect_object : out fpga_interconnect_record);
+    procedure init_bus (
+        signal bus_out : out fpga_interconnect_record);
 ------------------------------------------------------------------------
-    function fpga_interconnect_is_ready (fpga_interconnect_object : fpga_interconnect_record)
+    procedure write_data_to_address (
+        signal bus_out : out fpga_interconnect_record;
+        address : integer;
+        data : integer);
+------------------------------------------------------------------------
+    function write_to_address_is_requested (
+        bus_in : fpga_interconnect_record;
+        address : integer)
+    return boolean;
+------------------------------------------------------------------------
+    function get_data ( bus_in : fpga_interconnect_record)
+        return integer;
+------------------------------------------------------------------------
+    procedure request_data_from_address (
+        signal bus_out : out fpga_interconnect_record;
+        address : integer);
+------------------------------------------------------------------------
+    function data_is_requested_from_address (
+        bus_in : fpga_interconnect_record;
+        address : integer)
         return boolean;
 ------------------------------------------------------------------------
+    procedure connect_data_to_address (
+        bus_in         : in fpga_interconnect_record  ;
+        signal bus_out : out fpga_interconnect_record ;
+        address        : in integer                   ;
+        signal data    : inout integer);
+------------------------------------------------------------------------
+    procedure connect_read_only_data_to_address (
+        bus_in         : in fpga_interconnect_record  ;
+        signal bus_out : out fpga_interconnect_record ;
+        address        : in integer                   ;
+        data           : in integer);
+------------------------------------------------------------------------
+
 end package fpga_interconnect_pkg;
 
 package body fpga_interconnect_pkg is
-------------------------------------------------------------------------
-    procedure create_fpga_interconnect 
-    (
-        signal fpga_interconnect_object : inout fpga_interconnect_record
-    ) 
-    is
-        alias fpga_interconnect_is_requested is fpga_interconnect_object.fpga_interconnect_is_requested;
-        alias fpga_interconnect_is_done is fpga_interconnect_object.fpga_interconnect_is_done;
-    begin
-        fpga_interconnect_is_requested <= false;
-        if fpga_interconnect_is_requested then
-            fpga_interconnect_is_done <= true;
-        else
-            fpga_interconnect_is_done <= false;
-        end if;
-    end procedure;
 
 ------------------------------------------------------------------------
-    procedure request_fpga_interconnect
+    function "and"
     (
-        signal fpga_interconnect_object : out fpga_interconnect_record
+        left, right : fpga_interconnect_record
+    )
+    return fpga_interconnect_record
+    is
+    begin
+    return (left.data                           and right.data                          ,
+            left.address                        and right.address                       ,
+            left.data_write_is_requested_with_0 and right.data_write_is_requested_with_0,
+            left.data_read_is_requested_with_0  and right.data_read_is_requested_with_0 );
+        
+    end "and";
+------------------------------------------------------------------------
+    function to_integer
+    (
+        data : std_logic_vector 
+    )
+    return integer
+    is
+    begin
+        return to_integer(unsigned(data));
+    end to_integer;
+------------------------------------------------------------------------
+    function to_std_logic_vector
+    (
+        data : integer
+    )
+    return std_logic_vector
+    is
+    begin
+        return std_logic_vector(to_unsigned(data, 16));
+        
+    end to_std_logic_vector;
+------------------------------------------------------------------------
+    procedure init_bus
+    (
+        signal bus_out : out fpga_interconnect_record
     ) is
     begin
-        fpga_interconnect_object.fpga_interconnect_is_requested <= true;
-        
-    end request_fpga_interconnect;
-
+        bus_out <= init_fpga_interconnect;
+    end init_bus;
 ------------------------------------------------------------------------
-    function fpga_interconnect_is_ready
+    procedure write_data_to_address
     (
-        fpga_interconnect_object : fpga_interconnect_record
+        signal bus_out : out fpga_interconnect_record;
+        address : integer;
+        data : integer
+    ) is
+    begin
+        bus_out.address <= to_std_logic_vector(address);
+        bus_out.data    <= to_std_logic_vector(data);
+        bus_out.data_write_is_requested_with_0 <= '0';
+    end write_data_to_address;
+------------------------------------------------------------------------
+    function write_to_address_is_requested
+    (
+        bus_in : fpga_interconnect_record;
+        address : integer
     )
     return boolean
     is
     begin
-        return fpga_interconnect_object.fpga_interconnect_is_done;
-    end fpga_interconnect_is_ready;
-
+        
+        return bus_in.data_write_is_requested_with_0 = '0' and
+            to_integer(bus_in.address) = address;
+    end write_to_address_is_requested;
 ------------------------------------------------------------------------
+    function get_data
+    (
+        bus_in : fpga_interconnect_record
+    )
+    return integer
+    is
+    begin
+        return to_integer(bus_in.data);
+    end get_data;
+------------------------------------------------------------------------
+    procedure request_data_from_address
+    (
+        signal bus_out : out fpga_interconnect_record;
+        address : integer
+    ) is
+    begin
+        bus_out.data_read_is_requested_with_0 <= '0';
+        bus_out.address <= to_std_logic_vector(address);
+    end request_data_from_address;
+------------------------------------------------------------------------
+    function data_is_requested_from_address
+    (
+        bus_in : fpga_interconnect_record;
+        address : integer
+    )
+    return boolean
+    is
+    begin
+        return bus_in.data_read_is_requested_with_0 = '0' and
+            to_integer(bus_in.address) = address;
+    end data_is_requested_from_address;
+------------------------------------------------------------------------
+    procedure connect_data_to_address
+    (
+        bus_in         : in fpga_interconnect_record  ;
+        signal bus_out : out fpga_interconnect_record ;
+        address        : in integer                   ;
+        signal data    : inout integer
+    ) is
+    begin
+        if write_to_address_is_requested(bus_in, address) then
+            data <= get_data(bus_in);
+        end if;
+
+        if data_is_requested_from_address(bus_in, address) then
+            write_data_to_address(bus_out, 0, data);
+        end if;
+        
+    end connect_data_to_address;
+------------------------------------------------------------------------
+    procedure connect_read_only_data_to_address
+    (
+        bus_in         : in fpga_interconnect_record  ;
+        signal bus_out : out fpga_interconnect_record ;
+        address        : in integer                   ;
+        data           : in integer
+    ) is
+    begin
+        if data_is_requested_from_address(bus_in, address) then
+            write_data_to_address(bus_out, 0, data);
+        end if;
+        
+    end connect_read_only_data_to_address;
+------------------------------------------------------------------------
+
+
 end package body fpga_interconnect_pkg;
--- fpga_interconnect_simulation/tb_fpga_interconnect.vhd
+++ fpga_interconnect_simulation/tb_fpga_interconnect.vhd
@@ -3,6 +3,8 @@ LIBRARY ieee  ;
     USE ieee.std_logic_1164.all  ; 
     use ieee.math_real.all;
 
+    use work.fpga_interconnect_pkg.all;
+
 library vunit_lib;
     use vunit_lib.run_pkg.all;
 
@@ -22,6 +24,16 @@ architecture vunit_simulation of tb_fpga_interconnect is
     -----------------------------------
     -- simulation specific signals ----
 
+    signal bus_from_stimulus : fpga_interconnect_record := init_fpga_interconnect;
+    signal data_in_stimulus : integer := 0;
+
+    signal bus_from_endpoint_1 : fpga_interconnect_record := init_fpga_interconnect;
+    signal data_in_endpoint_1 : integer := 0;
+
+    signal bus_from_endpoint_2 : fpga_interconnect_record := init_fpga_interconnect;
+    signal data_in_endpoint_2 : integer := 0;
+
+    signal combined_bus : fpga_interconnect_record := init_fpga_interconnect;
 begin
 
 ------------------------------------------------------------------------
@@ -54,8 +66,56 @@ begin
         if rising_edge(simulator_clock) then
             simulation_counter <= simulation_counter + 1;
 
+            init_bus(bus_from_stimulus);
+
+            CASE simulation_counter is
+                WHEN 5 =>  write_data_to_address(bus_from_stimulus      , 1             , 35   );
+                WHEN 6 =>  write_data_to_address(bus_from_stimulus      , 1             , 46   );
+                WHEN 7 =>  write_data_to_address(bus_from_stimulus      , 1             , 25   );
+                WHEN 8 =>  write_data_to_address(bus_from_stimulus      , 2             , 5895 );
+                WHEN 9 =>  request_data_from_address(bus_from_stimulus  , 1                    );
+                WHEN 10 =>  request_data_from_address(bus_from_stimulus , 2                    );
+                WHEN 11 =>  request_data_from_address(bus_from_stimulus , 3                    );
+                WHEN 12 =>  write_data_to_address(bus_from_stimulus     , 3             , 0    );
+                WHEN 13 =>  request_data_from_address(bus_from_stimulus , 3                    );
+                WHEN others => --do nothing
+            end CASE; --simulation_counter
+
+            connect_data_to_address(combined_bus, bus_from_stimulus, 0, data_in_stimulus);
 
         end if; -- rising_edge
     end process stimulus;	
+------------------------------------------------------------------------
+    combine_buses : process(simulator_clock)
+        
+    begin
+        if rising_edge(simulator_clock) then
+            combined_bus <= bus_from_endpoint_2 and bus_from_endpoint_1;
+
+        end if; --rising_edge
+    end process combine_buses;	
+------------------------------------------------------------------------
+    endpoint_1 : process(simulator_clock)
+        
+    begin
+        if rising_edge(simulator_clock) then
+
+            init_bus(bus_from_endpoint_1);
+            connect_data_to_address(bus_from_stimulus, bus_from_endpoint_1, 1 , data_in_endpoint_1);
+
+        end if; --rising_edge
+    end process endpoint_1;	
+------------------------------------------------------------------------
+    endpoint_2 : process(simulator_clock)
+        
+    begin
+        if rising_edge(simulator_clock) then
+
+            init_bus(bus_from_endpoint_2);
+            connect_data_to_address(bus_from_stimulus, bus_from_endpoint_2, 2 , data_in_endpoint_2);
+            connect_read_only_data_to_address(bus_from_stimulus, bus_from_endpoint_2, 3 , 35896);
+
+        end if; --rising_edge
+    end process endpoint_2;	
 ------------------------------------------------------------------------
 end vunit_simulation;
