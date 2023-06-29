4e087715363865085f4037c595667b035fb688a5
Jari Honkanen <jaarhonka@yahoo.com>
2023-06-29 11:50:33 +0300

    add paramter for calling with submodule path

--- ghdl_compile_fpga_interconnect.bat
+++ ghdl_compile_fpga_interconnect.bat
@@ -1,5 +1,5 @@
 echo off
 
-ghdl -a --ieee=synopsys --std=08 interconnect_configuration/data_15_address_15_bit_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 fpga_interconnect_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 bus_controller/bus_controller_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %1/interconnect_configuration/data_15_address_15_bit_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %1/fpga_interconnect_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 %1/bus_controller/bus_controller_pkg.vhd
